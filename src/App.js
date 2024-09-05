ef63467f13fefed54e64a983d5800aa439345d14
rmsetty <rajiv.dfw@gmail.com>
2024-09-05 16:03:33 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import WaitList from "./components/waitList";
+import NavBar from "./components/navbar";
+import LandingPage1 from './components/landingPage1';
+import InfoSection from './components/infoSection';
+import Footer from './components/footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <WaitList />
+        <NavBar />
+        <LandingPage1 />
+        <InfoSection />
+        <Footer/>
     </div>
   );
 }
